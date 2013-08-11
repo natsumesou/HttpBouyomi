57b645569f7ce67d5471cfba210a559e33024a5a
natsumesou <natsumesouxx@gmail.com>
2013-08-11 20:19:12 +0900

    fix script

--- Bouyomi.js
+++ Bouyomi.js
@@ -1,11 +1,16 @@
-document.addEventListener("DOMNodeInserted", function(e) {
+document.addEventListener('DOMNodeInserted', function(e) {
     var line = e.target;
-    switch (line.getAttribute("type")) {
-        case "privmsg":
+
+    if(line.getAttribute('nick') === 'jtv') {
+        return false;
+    }
+
+    switch (line.getAttribute('type')) {
+        case 'privmsg':
             var msg = line.getElementsByClassName('message').item(0);
             var text = msg.innerHTML;
             var client = new XMLHttpRequest();
-            client.open('GET', 'http://natsu.me:9292/?text=' + text, true);
+            client.open('GET', 'http://localhost:9292/?text=' + text, true);
             client.onreadystatechange = function() {
                 if(client.readyState != 4) {
                     console.error('error');
